26b45d8a376d4897f227a1d94207467ded453ff1
Choun Rathanak <Nak4131@gmail.com>
2025-12-19 11:15:19 +0700

    push

--- lib/ui/students/students.dart
+++ lib/ui/students/students.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
-
+import 'student_details.dart';
 class Students extends StatelessWidget {
   const Students({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final students = [
+      'Student A',
+      'Student B',
+      'Student C',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Students')),
+      body: ListView.builder(
+        itemCount: students.length,
+        itemBuilder: (context, index) {
+          final student = students[index];
+
+          return ListTile(
+            leading: const Icon(Icons.class_),
+            title: Text(student),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              // Go to the  StudentDetails
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      StudentDetails(studentId: student),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
